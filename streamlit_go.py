005ef2f42034da6e817c18c9402d44d10c1c5fd1
sungkyung-park <75871694+sungkyung-park@users.noreply.github.com>
2024-02-06 14:09:33 +0900

    Update streamlit_go.py

--- streamlit_go.py
+++ streamlit_go.py
@@ -25,6 +25,7 @@ from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
 )
 
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def main():
     st.set_page_config(
@@ -32,8 +33,7 @@ def main():
     page_icon=":robot_face:")
 
     st.title("_당신의 혈압관리 도우미 :red[Go_manager]_ :robot_face:")
-    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-    
+
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
